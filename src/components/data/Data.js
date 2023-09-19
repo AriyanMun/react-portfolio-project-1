d36c65fbb094912c08bd081618e2689155780a89
AriyanMun <ariyanmunshi98@gmail.com>
2023-09-19 17:26:24 +0530

    New commit

---
+++ src/components/data/Data.js
@@ -0,0 +1,318 @@
+export const nav = [
+  {
+    text: "home",
+    path: "/",
+  },
+  {
+    text: "about",
+    path: "/about",
+  },
+  {
+    text: "services",
+    path: "/services",
+  },
+  {
+    text: "blog",
+    path: "/blog",
+  },
+  {
+    text: "pricing",
+    path: "/pricing",
+  },
+  {
+    text: "contact",
+    path: "/contact",
+  },
+]
+export const featured = [
+  {
+    cover: "../images/hero/h1.png",
+    name: "Family House",
+    total: "122 Property",
+  },
+  {
+    cover: "../images/hero/h2.png",
+    name: "House & Villa",
+    total: "155 Property",
+  },
+  {
+    cover: "../images/hero/h3.png",
+    name: "Apartment",
+    total: "300 Property",
+  },
+  {
+    cover: "../images/hero/h4.png",
+    name: "Office & Studio",
+    total: "80 Property",
+  },
+  {
+    cover: "../images/hero/h6.png",
+    name: "Villa & Condo",
+    total: "80 Property",
+  },
+]
+export const list = [
+  {
+    id: 1,
+    cover: "../images/list/p-1.png",
+    name: "Califoria Standand Package",
+    location: "California, USA",
+    category: "Basic",
+    price: "$3,700",
+    type: "National",
+  },
+  {
+    id: 2,
+    cover: "../images/list/p-2.png",
+    name: "New York Basic Package",
+    location: "New York, USA",
+    category: "Basic",
+    price: "$3200",
+    type: "National",
+  },
+  {
+    id: 3,
+    cover: "../images/list/p-7.png",
+    name: "Bangkok International Package",
+    location: "Malaysia",
+    category: "Standard",
+    price: "$5,860",
+    type: "International",
+  },
+  {
+    id: 4,
+    cover: "../images/list/p-4.png",
+    name: "Japan International Package",
+    location: "Japan",
+    category: "Standard",
+    price: "$7,540",
+    type: "International",
+  },
+  {
+    id: 5,
+    cover: "../images/list/p-5.png",
+    name: "Philipines International Package",
+    location: "Philipines",
+    category: "Standard",
+    price: "$6600",
+    type: "International",
+  },
+  {
+    id: 6,
+    cover: "../images/list/p-6.png",
+    name: "Greece International Package",
+    location: "Greece",
+    category: "Exclusive",
+    price: "$10,000",
+    type: "International",
+  },
+]
+export const awards = [
+  {
+    icon: <i class='fa-solid fa-trophy'></i>,
+    num: "32 M	",
+    name: "Blue Burmin Award",
+  },
+  {
+    icon: <i class='fa-solid fa-briefcase'></i>,
+    num: "43 M",
+    name: "Mimo X11 Award",
+  },
+  {
+    icon: <i class='fa-solid fa-lightbulb'></i>,
+    num: "51 M",
+    name: "Australian UGC Award",
+  },
+  {
+    icon: <i class='fa-solid fa-heart'></i>,
+    num: "42 M",
+    name: "IITCA Green Award",
+  },
+]
+export const location = [
+  {
+    id: 1,
+    name: "New Orleans, Louisiana",
+    Villas: "12 Villas",
+    Apartments: "10 Apartments",
+    Offices: "07 Offices",
+    cover: "./images/location/city-1.png",
+  },
+  {
+    id: 2,
+    name: "Jerrsy, United State",
+    Villas: "12 Villas",
+    Apartments: "10 Apartments",
+    Offices: "07 Offices",
+    cover: "./images/location/city-2.png",
+  },
+  {
+    id: 3,
+    name: "Liverpool, London",
+    Villas: "12 Villas",
+    Apartments: " 10 Apartments",
+    Offices: "07 Offices",
+    cover: "./images/location/city-3.png",
+  },
+  {
+    id: 4,
+    name: "NewYork, United States",
+    Villas: "12 Villas",
+    Apartments: " 10 Apartments",
+    Offices: "07 Offices",
+    cover: "./images/location/city-4.png",
+  },
+  {
+    id: 5,
+    name: "Montreal, Canada",
+    Villas: "12 Villas",
+    Apartments: " 10 Apartments",
+    Offices: "07 Offices",
+    cover: "./images/location/city-5.png",
+  },
+  {
+    id: 6,
+    name: "California, USA",
+    Villas: "12 Villas",
+    Apartments: " 10 Apartments",
+    Offices: "07 Offices",
+    cover: "./images/location/city-6.png",
+  },
+]
+export const team = [
+  {
+    list: "50",
+    cover: "../images/customer/team-1.jpg",
+    address: "Liverpool, Canada",
+    name: "Sargam S. Singh",
+    icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+  },
+  {
+    list: "70",
+    cover: "../images/customer/team-2.jpg",
+    address: "Montreal, Canada",
+    name: "Harijeet M. Siller",
+    icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+  },
+  {
+    list: "80",
+    cover: "../images/customer/team-3.jpg",
+    address: "Denever, USA",
+    name: "Anna K. Young",
+    icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+  },
+  {
+    list: "51",
+    cover: "../images/customer/team-4.jpg",
+    address: "2272 Briarwood Drive",
+    name: "Michael P. Grimaldo",
+    icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+  },
+  {
+    list: "42",
+    cover: "../images/customer/team-5.jpg",
+    address: "2272 Briarwood Drive",
+    name: "Michael P. Grimaldo",
+    icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+  },
+  {
+    list: "38",
+    cover: "../images/customer/team-5.jpg",
+    address: "Montreal, USA",
+    name: "Adam K. Jollio",
+    icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+  },
+]
+export const price = [
+  {
+    plan: "Basic",
+    price: "2900-5500",
+    ptext: "per user, per night",
+    list: [
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "includes pan-american trips only",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "stay at modern, maintained ,and good stays",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "food included",
+      },
+      { change: "color", icon: <i class='fa-solid fa-x'></i>, text: "Personal Help Support" },
+      { change: "color", icon: <i class='fa-solid fa-x'></i>, text: "Passport support" },
+    ],
+  },
+  {
+    best: "Best Value",
+    plan: "Standard",
+    price: "5,500-7,900",
+    ptext: "per user, per night",
+    list: [
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "includes asian countries",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "stay at modern, maintained ,and good stays",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "food included",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "Personal Help Support",
+      },
+      {
+        change: "color",
+        icon: <i class='fa-solid fa-x'></i>,
+        text: "Passport support",
+      },
+    ],
+  },
+  {
+    plan: "Platinum",
+    price: "7,900-10,000",
+    ptext: "per user, per night",
+    list: [
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "includes european countries",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "stay at only 3-5 star properties",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "food included",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "Personal Help Support",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "passport support",
+      },
+    ],
+  },
+]
+export const footer = [
+  {
+    title: "LAYOUTS",
+    text: [{ list: "Home Page" }, { list: "About Page" }, { list: "Service Page" }, { list: "Property Page" }, { list: "Contact Page" }, { list: "Single Blog" }],
+  },
+  {
+    title: "ALL SECTIONS",
+    text: [{ list: "Headers" }, { list: "Features" }, { list: "Attractive" }, { list: "Testimonials" }, { list: "Videos" }, { list: "Footers" }],
+  },
+  {
+    title: "COMPANY",
+    text: [{ list: "About" }, { list: "Blog" }, { list: "Pricing" }, { list: "Affiliate" }, { list: "Login" }, { list: "Changelog" }],
+  },
+]
